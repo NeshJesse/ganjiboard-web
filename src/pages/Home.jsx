a8439d938d57af0fc751971264bf63950baa53e7
SlipKitchen <neshonyango@gmail.com>
2025-09-14 19:43:11 +0300

    some seo mods

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ import { useState, useEffect } from "react";
 import { ensureInitialized, saveBoardState, loadBoardState, clearBoardState, exportBoardData, importBoardData, createBoard, listBoards, setActiveBoardId, loadBoardById, renameBoard, deleteBoard } from "../utils/storage";
 
 
-export default function App() {
+export default function Home() {
   const [currentTool, setCurrentTool] = useState("select");
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [cards, setCards] = useState([]);
